8f92ac762be1149408e6456c4e3a6f0fed9e8a72
Eric Seyden <ericseyden@yahoo.com>
2023-03-10 11:47:20 -0700

    document sudoku

--- homework/homework5/SudokuChecker.java
+++ homework/homework5/SudokuChecker.java
@@ -1,4 +1,15 @@
-import java.awt.desktop.OpenFilesEvent;
+/**
+ * -------------------------------------------------------------------------------
+ * CSCI 152, Homework #5
+ * Problem 2
+ * (SudokuChecker.java)
+ * -------------------------------------------------------------------------------
+ * This program reads a sudoku solution from a file and checks if it is valid.
+ * -------------------------------------------------------------------------------
+ * Eric Seyden
+ * 2023-03-10
+ * -------------------------------------------------------------------------------
+ */
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
@@ -7,63 +18,92 @@ import java.util.Scanner;
 public class SudokuChecker {
 
     public static void main(String[] args) {
+        // Use a 10x10 array for holding the state of the Sudoku puzzle
         int[][] SudokuSolution = new int[9][9];
+
+        //Check for file name argument
         if(args.length != 1) {
             System.out.println("Please specify file name for sudoku solution.");
             System.exit(1);
         }
-        Scanner SudokuFileScanner = OpenFile(args[0]);
+
+
+        Scanner SudokuFileScanner = OpenFile(args[0]); // Open file for reading
         int lineCount = 0;
-        while(SudokuFileScanner.hasNextLine()) {
+        while(SudokuFileScanner.hasNextLine()) { // read sudoku solution file into array
             SudokuSolution[lineCount] = SpacedIntsToArray(SudokuFileScanner.nextLine());
             lineCount++;
         }
 
+        // Store puzzle into 9 3x3 matrices
         int[][][] MiniGrids = new int[9][3][3];
+        // Loop through all possible 3x3 grids
         for(int SubArrayCount = 0; SubArrayCount < 9; SubArrayCount++ ){
+            // Fill out a 3x3
             for(int y=0; y<3; y++) {
                 for(int x=0; x<3; x++) {
+                    // Figure out offset x/y to read from for each sub matrix
                     int suby = y + ((SubArrayCount / 3) * 3);
                     int subx = x + ((SubArrayCount % 3) * 3);
+                    // Store the value in a sub matrix
                     MiniGrids[SubArrayCount][y][x] = SudokuSolution[suby][subx];
                 }
             }
         }
 
-        boolean valid = true;
+        boolean valid = true; // We haven't lost yet
 
+        // Use one loop for checking rows and columns
         for(int c1=0; c1<9; c1++) {
+            // Init arrays to store if we found that value
             boolean[] foundRow = new boolean[9];
             boolean[] foundColumn = new boolean[9];
+            // Loop through row/column values
             for(int c2=0; c2<9; c2++) {
+                // Read value from puzzle matrix and translate it to array index
                 int rowValueIndex = SudokuSolution[c1][c2] - 1;
                 int columnValueIndex = SudokuSolution[c2][c1] - 1;
-                if(foundRow[rowValueIndex]) {
-                    valid = false;
+                // Check if value has already been indexed
+                if(foundRow[rowValueIndex] || foundColumn[columnValueIndex]) {
+                    valid = false; // Flag puzzle as not valid
+                    break; // Puzzle invalid stop checking
                 } else {
+                    // Flip boolean to true for the index value of a puzzle answer for row and column
                     foundRow[rowValueIndex] = true;
                     foundColumn[columnValueIndex] = true;
                 }
             }
+            if(!valid) break; // Slight optimization for puzzle that gets is invalid.
         }
 
-        for(int[][] Grid: MiniGrids) {
+        // Loop through the 3x3 answer grids if puzzle hasn't been invalidated
+        if(!valid) for(int[][] Grid: MiniGrids) {
+            // Boolean array for values found in each mini grid
             boolean[] found = new boolean[9];
+            // Loop through the 3x3
             for(int c1=0; c1<3; c1++) {
                 for(int c2=0; c2<3; c2++) {
+                    // Store found value
                     int valueIndex = Grid[c1][c2] - 1;
-                    if(found[valueIndex]) {
-                        valid = false;
+                    if(found[valueIndex]) { // We found a duplicate
+                        valid = false; // Fail puzzle validation
                     } else {
-                        found[valueIndex] = true;
+                        found[valueIndex] = true; // Store true at index of puzzle value
                     }
                 }
             }
         }
 
-        System.out.println(valid);
+        if(valid) System.out.println("Valid");
+        else System.out.println("Invalid");
 
     }
+
+    /**
+     * Open File
+     * @param filename File Name
+     * @return Scanner
+     */
     public static Scanner OpenFile(String filename) {
         File FileHandle = new File(filename);
         try {
@@ -91,6 +131,10 @@ public class SudokuChecker {
 
     }
 
+    /**
+     * Fun for checking the mini grid algo
+     * @param Matrix
+     */
     static void print(int[][] Matrix) {
         StringBuilder content = new StringBuilder();
         for(int[] row: Matrix) {
